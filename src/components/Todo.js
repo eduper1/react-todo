013c25a25b25038a4541b1e937095436edb6f56a
eduper1 <eduper.havard@gmail.com>
2023-07-06 14:50:21 +0300

    toggling todo templates

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,10 +13,14 @@ function Todo(props) {
       <input id={props.id} className="todo-text" type="text" />
     </div>
     <div className="btn-group">
-      <button type="button" className="btn todo-cancel">
-        Cancel
-        <span className="visually-hidden">renaming {props.name}</span>
-      </button>
+      <button
+  type="button"
+  className="btn todo-cancel"
+  onClick={() => setEditing(false)}>
+  Cancel
+  <span className="visually-hidden">renaming {props.name}</span>
+</button>
+
       <button type="submit" className="btn btn__primary todo-edit">
         Save
         <span className="visually-hidden">new name for {props.name}</span>
@@ -38,9 +42,10 @@ const viewTemplate = (
       </label>
     </div>
     <div className="btn-group">
-      <button type="button" className="btn">
-        Edit <span className="visually-hidden">{props.name}</span>
-      </button>
+      <button type="button" className="btn" onClick={() => setEditing(true)}>
+  Edit <span className="visually-hidden">{props.name}</span>
+</button>
+
       <button
         type="button"
         className="btn btn__danger"
@@ -52,29 +57,8 @@ const viewTemplate = (
 );
 
 
-    return (
-      // …
-      <li className="todo stack-small">
-          <div className="c-cb">
-            <input id={props.id} type="checkbox" defaultChecked={props.completed} onChange={() => props.toggleTaskCompleted(props.id)} />
-            <label className="todo-label" htmlFor={props.id}>
-              {props.name}
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
-            </button>
-            <button
-  type="button"
-  className="btn btn__danger"
-  onClick={() => props.deleteTask(props.id)}>
-  Delete <span className="visually-hidden">{props.name}</span>
-</button>
+    return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 
-          </div>
-        </li>
-    );
   }
   
   export default Todo;
